69c50a3c1f2ca5d5092c6bcd58c372ba16c1acab
naman2341 <38250182+naman2341@users.noreply.github.com>
2021-03-02 05:19:29 +0530

    Level 1 function added

--- Naman_puzzle.py
+++ Naman_puzzle.py
@@ -1,14 +1,54 @@
 import random
 import time
 
+global char
+
 def move_on():
 	pass
 def game_over():
 	pass
 
+#Level 1
+def level1():
+	var=0
+	print("Alice is reading a book.")
+	time.sleep(0.7*var)
+	print("The weather outside is dark and gloomy.")
+	time.sleep(1.5*var)
+	print("It’s raining heavily and she is alone at home.")
+	time.sleep(1.5*var)
+	print("She hears a thud on the door and gets out of bed to check if it was a stray cat in need of shelter from the harsh rain.")
+	time.sleep(6*var)
+	print("However, she finds herself across a shady person holding a gun.")
+	time.sleep(4.5*var)
+	print("The person opens fire on Alice before she could cry for help and she’s dead on the spot.")
+	time.sleep(6*var)
+	print("Police investigation begins the next day and the case is assigned to you (Elyse/Kevin).")
+	time.sleep(5.25*var)
+	print("The player is filled in with the details and the gameplay begins.")
+	time.sleep(3.5*var)
+	a=1
+	op=input("Are you ready to solve this case?\n")
+	op=op.lower()
+	if op=='y' or op=='yes':
+		a=0
+	while a:
+		print("Are you sure you arn't excited to go on this amazing adventure!!?")
+		op=input("So are you ready to solve this case?\n")
+		op=op.lower()
+		if op=='y' or op=='yes':
+			a=0
+	print("That is great let the journey begin!")
+	op=input("Character? (Elyse/Kevin)\n")
+	op=op.lower()
+	if op=='elyse':
+		char=True
+	else:
+		char=False
+
+level1()
 
 #Level 6
-
 def level6_puz():
 	pa_l6=1
 	print("Looks like you have forgot your math! Time to test your basic math knowlege!")
@@ -29,7 +69,7 @@ def level6_puz():
 		move_on()
 
 print("Level 6")
-level6_puz()
+#level6_puz()
 
 #Timed input
 def input_time():
@@ -70,5 +110,6 @@ def level7_puz():
 	else:
 		print("Looks like you have passed this puzzle too!")
 		move_on()
+
 print("Level 7")
-level7_puz()
+#level7_puz()
