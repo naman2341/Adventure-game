e46ce0a05a63d6041ae750c1572d433e0de563f2
Nameeta Kuruwatti <72734011+nameetak-22@users.noreply.github.com>
2021-03-02 21:22:03 +0530

    Update Nameeta_puzzle.py
    level5 added

--- Nameeta_puzzle.py
+++ Nameeta_puzzle.py
@@ -33,6 +33,27 @@ def keira_level4_puz():
     return pa_l4
 
 res=keira_level4_puz()
+
+
+
+#level 5
+def level5_puz():
+    pa_l5=1
+    print("Are you ready to search nooks and corners for the best clue to solve the case?!!")
+    s= r"Type the word that comes before 'murder'"
+    print(s)
+    print('The “locked-room” mystery: This is a type of detective story in which a murder is committed under impossible conditions, usually in a place that the murderer couldn’t have entered or left. Although he isn’t given enough credit for it, Doyle was one of the earliest innovators of this classic subgenre of crime fiction. Examples of this type include “The Speckled Band,” “The Empty House,” and The Valley of Fear. ')
+    time.sleep(1)
+    ans=input()
+    if(ans!='a'):
+        pa_l5=0
+    if pa_l4>0:
+        print("Next level")
+    else:
+        print("YOu lost a life")
+    
+level5_puz()
+
 if res>0:
     print("Next level")
 else:
