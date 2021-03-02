9f953db04243b77c2360695d7c64315950f03e64
naman2341 <38250182+naman2341@users.noreply.github.com>
2021-03-03 04:51:12 +0530

    added level2,Big change
    Level intro,1,2,6,7

--- Naman_puzzle.py
+++ Naman_puzzle.py
@@ -2,18 +2,32 @@ import random
 import time
 
 char=True
-var=1
+var=0 #Changed to 0 for testing
+BOH=3
 
 def move_on():
 	pass
-def game_over():
-	pass
+
+def game_over(): #Still WIP
+	global BOH
+	print("You lost a badge of honour")
+	BOH-=1
+	if BOH<=0:
+		print("Oh no! Now you have lost all of your Badge of Honours")
+		print("You cannot be allowed to continue this investigation further!")
+		exit()
+
+def end_game():
+	print("Oh looks like you did not want to continue further")
+	print("Thank you for playing")
+	exit()
 
 def intro():
 	print(format('Welcome to "Murder in New York"','^100'))
 	print("This is a text based adventure game consisting of several levels and challenges as the story progresses")
 	print("We hope you enjoy!")
 	time.sleep(5*var)
+
 #Level 1
 def level1():
 	global char
@@ -39,14 +53,8 @@ def level1():
 	a=1
 	op=input("Are you ready to solve this case?\n")
 	op=op.lower()
-	if op=='y' or op=='yes':
-		a=0
-	while a:
-		print("Are you sure you arn't excited to go on this amazing adventure!!?")
-		op=input("So are you ready to solve this case?\n")
-		op=op.lower()
-		if op=='y' or op=='yes':
-			a=0
+	if op!='y' and op!='yes':
+		end_game()
 	print("That is great let the journey begin!")
 	op=input("Character? (Elyse/Kevin)\n")
 	op=op.lower()
@@ -55,22 +63,86 @@ def level1():
 	else:
 		char=False
 
-#level1()
+#Level 2 Puzzle
+def level2_puz():
+	pa_l2=1
+	print("That is great! Lets start with Ethan")
+	#boyfriend,suspicious-Ethan
+	#bestfriend,friendly-Kiera
+	#Words might have to be changed probabily
+	print("Looks like Alice loved writing about her friends in her diary , but looks like its scrambelled!")
+	print("Start by unscrambling these clues related to Ethan")
+	while 1:
+		str1="boyfriend"
+		str2="suspicious"
+		l1=list(str1)
+		l2=list(str2)
+		random.shuffle(l1)
+		random.shuffle(l2)
+		st1="".join(l1)
+		st2="".join(l2)
+		if st1[0]!=str1[0] and st2[0]!=str2[0]:
+			break
+	print(st1)
+	in1=input("The unscrambled word is:")
+	print(st2)
+	in2=input("The unscrambled word is:")
+	if (in1!=str1 or in2!=str2) and (in1!="cheat" or in2!="cheat"):
+		pa_l2=0
 
-#Level 6
-def level6_puz():
+	print("Now time to unscramble the clues related to Kiera")
+	while 1:
+		str1="bestfriend"
+		str2="friendly"
+		l1=list(str1)
+		l2=list(str2)
+		random.shuffle(l1)
+		random.shuffle(l2)
+		st1="".join(l1)
+		st2="".join(l2)
+		if st1[0]!=str1[0] and st2[0]!=str2[0]:
+			break
+	print(st1)
+	in1=input("The unscrambled word is:")
+	print(st2)
+	in2=input("The unscrambled word is:")
+	if (in1!=str1 or in2!=str2) and (in1!="cheat" or in2!="cheat"):
+		pa_l2=0
+	if pa_l2==0:
+		print("Oops, looks like your answer was wrong!")
+		game_over()
+	else:
+		print("Looks like you have passed this puzzle")
+		move_on()
+
+#Level 2
+def level2():
 	print('\n\n')
-	print(format("LEVEL 6",'^100'))
+	print(format("LEVEL 2",'^100'))
 	print('\n')
+	print("The victim’s family is enquired about Alice’s recent behaviour.")
+	time.sleep(4.5*var)
+	print("The father comments about her boyfriend Ethan and best friend Kiera.")
+	time.sleep(5*var)
+	a=input("Do you wish to investigate Ethan or Kiera?\n")
+	a=a.lower()
+	if a=='y' or a=='yes':
+		level2_puz()
+	else:
+		end_game()
+	
+#Level 6 Puzzle
+def level6_puz():
 	pa_l6=1
 	print("Looks like you have forgot your math! Time to test your basic math knowlege!")
 	for i in range(3):
 		a = random.randint(-100, 100)
 		b = random.randint(-100, 100)
-		ans = 1 #ans=a+b
-#Changed to 1 for testing!
+		ans = a+b
 		inp = int(input("What is the sum of {} and {}\n".format(a, b)))
-		if inp!=ans:
+		if inp==-999: #Cheat answer is -999
+			break
+		if inp!=ans: 
 			print("Oops looks like your answer is wrong")
 			pa_l6=0
 			break
@@ -79,6 +151,13 @@ def level6_puz():
 	else:
 		print("Looks like you have passed this puzzle too!")
 		move_on()
+
+#Level 6
+def level6():
+	print('\n\n')
+	print(format("LEVEL 6",'^100'))
+	print('\n')
+	level6_puz()
 
 #Timed input
 def input_time():
@@ -96,19 +175,20 @@ def input_time():
 		return a
 	else:
 		return 0
-#Level 7
+
+#Level 7 Puzzle
 def level7_puz():
-	print('\n\n')
-	print(format("LEVEL 7",'^100'))
-	print('\n')
 	pa_l7=1
 	print("Looks like you are getting late for you flight! Time to speed!")
+	time.sleep(3)
 	for i in range(5):
 		print("Enter the following character within 2 seconds")
 		time.sleep(2)
 		ch=random.choice("abcdefghijklmnopqrstuvwxyz")
 		print(ch)
 		a=input_time()
+		if 'ch' == a: #Cheat answer is ch
+			break
 		if a==0:
 			print("Oops looks like you were too slow!")
 			pa_l7=0
@@ -123,11 +203,19 @@ def level7_puz():
 		print("Looks like you have passed this puzzle too!")
 		move_on()
 
+#Level 7
+def level7():
+	print('\n\n')
+	print(format("LEVEL 7",'^100'))
+	print('\n')
+	level7_puz()
 
+#Main function calling other functions
 def main_game():
 	intro()
 	level1()
-	level6_puz()
-	level7_puz()
-
+	level2()
+	level6()
+	level7()
+	
 main_game()
