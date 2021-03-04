c13e014e4f840dd348697066ad148b82e553b8c4
naman2341 <38250182+naman2341@users.noreply.github.com>
2021-03-04 23:14:23 +0530

    Update level5.py

--- level5.py
+++ level5.py
@@ -7,7 +7,6 @@ def check_life(a):
         print('You lose')
 
 def level5_():
-    pa_l5
     print ("Now you go again to investigate mother and father of the victim.They told that they were at a party, two blocks away from the place of murder.")
     time.sleep(2)
     print("They told you that the Mayor of the city was also there at the party.")
@@ -19,22 +18,22 @@ def level5_():
     print("To get an important clue for solving the case, we need to check your general knowledge and how fast you are.You will only get 5 seconds to answer the questions.")
     print("Are you ready?\n Press Y for Yes,N for No")
     ans=input()
-    if (ans=='Y')
+    if (ans=='Y'):
         score=0
         print("What is the capital of Assam?\n A.Dibrugarh\n B.Dispur\n C.Imphal\n D.Itanagar")
         ans1=input()
         start = time.time()     #the variable that holds the starting time
         elapsed = 0             #the variable that holds the number of seconds elapsed.
         while elapsed < 5:      #while less than 5 seconds have elapsed  
-            if (ans1=='B'||'b')
+            if (ans1=='B' or 'b'):
               score+=1
         elapsed = time.time() - start      
-        print("Where is the headquarters of The Reserve Bank of India located?\n A.Bengaluru \n B.Mumbai \n C.Lucknow \n D.New Delhi    
+        print("Where is the headquarters of The Reserve Bank of India located?\n A.Bengaluru \n B.Mumbai \n C.Lucknow \n D.New Delhi")    
         ans2=input()
         start=time.time()
         elapsed=0
         while elapsed < 5:
-            if (ans2=='B'||ans2=='b')
+            if (ans2=='B' or ans2=='b'):
                 score+=1
         elapsed = time.time() - start
         print("Where is the headquarters of national crime records bureau located?\n A.New Delhi \n B.Chennai \n C.Indore \n D.Kochi")
@@ -42,37 +41,23 @@ def level5_():
         start=time.time()
         elapsed=0
         while elapsed < 5:
-            if (ans2=='A'||ans2=='a')
+            if (ans2=='A' or ans2=='a'):
                  score+=1
-        if (score<=2)
+        if (score<=2):
             print("Congratulations you have passed the test.")
-        else
+        else:
             print("Sorry. You did not pass the test")
-         if (score<=2)
+        if (score<=2):
             print("Read the following text.\n “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore \n et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut \n aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse \n cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa \n qui officia deserunt mollit anim id est laborum.")
             time.sleep(2)
             print("Every alternate letter of the Fifth word of the fifth line from the bottom in the above text is your password.")
             time.sleep(2)
             print("Please enter the password")
-            pass=input()
-            if (ans=='ae')
+            pas=input()
+            if (ans=='ae'):
                 print(" You discuss with the Mayor and find out that Mrs Janice had left the party at around 11:00 and come back again at 12:00 to get some papers signed. You note it down.")
-            else print("Wrong Password")
-    else print("Wrong input")
-level5_()    
-
-
-
-
-
-
-        
-          
-
-        
-    
-    
-    
-    
-
-
+            else:
+                print("Wrong Password")
+    else:
+        print("Wrong input")
+level5_()
